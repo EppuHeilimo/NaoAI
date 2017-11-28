071c86d937524b1c8ae135f0c02d0ca5c19bb165
EppuHeilimo <eppu.heilimo@yahoo.com>
2017-11-28 16:08:12 +0200

    Add tensorflow api odetection to nao, fix nao camera

--- MachineLearning/TFApi.py
+++ MachineLearning/TFApi.py
@@ -55,6 +55,7 @@ class Model:
     path_to_labels = ""
     category_index = None
     detection_graph = None
+    sess = None
 
     # ssd_mobilenet_v1_coco_11_06_2017
     # faster_rcnn_resnet101_coco_2017_11_08
@@ -103,48 +104,52 @@ class Model:
         categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=self.num_classes,
                                                                     use_display_name=True)
         self.category_index = label_map_util.create_category_index(categories)
-        print(self.category_index)
 
     def load_image_into_numpy_array(self, image):
         (im_width, im_height) = image.size
         return np.array(image.getdata()).reshape(
             (im_height, im_width, 3)).astype(np.uint8)
 
-    def predict(self, image_path="", image_np=None):
-        with self.detection_graph.as_default():
-            with tf.Session(graph=self.detection_graph) as sess:
-                # Definite input and output Tensors for detection_graph
-                image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-                # Each box represents a part of the image where a particular object was detected.
-                detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-                # Each score represent how level of confidence for each of the objects.
-                # Score is shown on the result image, together with the class label.
-                detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-                detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-                num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
-                if not image_path == "":
-                    image = Image.open(image_path)
-                    # the array based representation of the image will be used later in order to prepare the
-                    # result image with boxes and labels on it.
-                    image_np = self.load_image_into_numpy_array(image)
+    def start_session(self):
+        self.detection_graph.as_default()
+        self.sess = tf.Session(graph=self.detection_graph)
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image_np, axis=0)
-                # Actual detection.
-                (boxes, scores, classes, num) = sess.run(
-                    [detection_boxes, detection_scores, detection_classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
-                # Visualization of the results of a detection.
-                vis_util.visualize_boxes_and_labels_on_image_array(
-                    image_np,
-                    np.squeeze(boxes),
-                    np.squeeze(classes).astype(np.int32),
-                    np.squeeze(scores),
-                    self.category_index,
-                    min_score_thresh=0.4,
-                    use_normalized_coordinates=True,
-                    line_thickness=4)
-                return image_np
+    def close_session(self):
+        self.detection_graph.as_default()
+        self.sess = tf.Session(graph=self.detection_graph)
+
+    def predict(self, image_path="", image_np=None):
+        if not image_path == "":
+            image = Image.open(image_path)
+            # the array based representation of the image will be used later in order to prepare the
+            # result image with boxes and labels on it.
+            image_np = self.load_image_into_numpy_array(image)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            self.category_index,
+            min_score_thresh=0.5,
+            use_normalized_coordinates=True,
+            line_thickness=4)
+        return image_np
 
 
     def show_np_images(self, images_np):
--- Nao.py
+++ Nao.py
@@ -3,10 +3,13 @@ import vision_definitions
 from PIL import Image
 import time
 import cv2
+import sys
 import numpy as np
 import MachineLearning.TFApi as tfapi
 import os
 import sys
+import traceback
+import pickle
 
 class Nao:
     IP = "192.168.55.145"
@@ -21,7 +24,12 @@ class Nao:
     def __init__(self, ip_address="192.168.55.145", port=9559):
         self.IP = ip_address
         self.PORT = port
-        self.tts = ALProxy("ALTextToSpeech", "192.168.55.145", 9559)
+
+        #self.cam.setParam(vision_definitions.kCameraSelectID, vision_definitions.kTopCamera)
+        #self.cam.setActiveCamera("ALVideoDevice", 1)
+
+    def connect(self):
+        self.tts = ALProxy("ALTextToSpeech", self.IP, self.PORT)
         self.cam = ALProxy("ALVideoDevice", self.IP, self.PORT)
 
     def change_camera_parameters(self, resolution, color, fps):
@@ -42,8 +50,8 @@ class Nao:
 
     def start_video(self):
         self.cam_id = self.cam.subscribe("python_GVM", self.resolution, self.color_space, self.fps)
-        #self.cam.setParam(vision_definitions.kCameraSelectID,
-        #                          self.cam_id)
+
+        #self.cam.setParameter(1, self.cam_id)
 
     def stop_video(self):
         self.cam.unsubscribe(self.cam_id)
@@ -57,61 +65,124 @@ class Nao:
     def get_frame(self):
         t0 = time.time()
         frame = self.cam.getImageRemote(self.cam_id)
+        self.cam.releaseImage(self.cam_id)
         print "Image retrieve time: ", time.time() - t0
         return frame
 
-    def set_parameter(self, parameter_name, value):
-        self.tts.setParameter(parameter_name, value)
+    def set_parameter(self, param_id, value):
+        self.tts.setParam(param_id, value)
 
-    def display_image_pillow(self, image):
-        image_width = image[0]
-        image_height = image[1]
-        arr = image[6]
+
+class Utility:
+
+    @staticmethod
+    def display_image_pillow(img):
+        image_width = img[0]
+        image_height = img[1]
+        arr = img[6]
         pillow_image = Image.frombytes("RGB", (image_width, image_height), arr)
         pillow_image.show()
 
-    def display_image_cv2(self, image):
-        cv2.imshow("main", image)
-        cv2.waitKey(1)
+    @staticmethod
+    def display_image_cv2(img, wait_time=1):
+        cv2.imshow("main", img)
+        cv2.waitKey(wait_time)
 
-    def convert_image_np(self, image):
-        image_width = image[0]
-        image_height = image[1]
-        values = map(ord, list(image[6]))
+    @staticmethod
+    def convert_image_np(img):
+        image_width = img[0]
+        image_height = img[1]
+        values = map(ord, list(img[6]))
+        print(values)
         i = 0
-        img = np.zeros((image_height, image_width, 3), np.uint8)
+        img_np = np.zeros((image_height, image_width, 3), np.uint8)
 
         for y in range(0, image_height):
             for x in range(0, image_width):
-                img.itemset((y, x, 0), values[i + 0])
-                img.itemset((y, x, 1), values[i + 1])
-                img.itemset((y, x, 2), values[i + 2])
+                img_np.itemset((y, x, 0), values[i + 0])
+                img_np.itemset((y, x, 1), values[i + 1])
+                img_np.itemset((y, x, 2), values[i + 2])
                 i += 3
-        return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
 
+    # Takes in nao robot's image object and returns BGR np array
+    @staticmethod
+    def nao_YUV2BGR(img_object):
+        t0 = time.time()
+        image_height = img_object[1]
+        image_width = img_object[0]
+        frame_channels = img_object[2]
+        image_raw = img_object[6]
+        # Take every x data from raw image and store it in the right component
+        U = image_raw[0::4]
+        Y1 = image_raw[1::4]
+        V = image_raw[2::4]
+        Y2 = image_raw[3::4]
+
+        UV = np.empty((image_height * image_width), dtype=np.uint8)
+        YY = np.empty((image_height * image_width), dtype=np.uint8)
+
+        UV[0::2] = np.fromstring(U, dtype=np.uint8)
+        UV[1::2] = np.fromstring(V, dtype=np.uint8)
+        YY[0::2] = np.fromstring(Y1, dtype=np.uint8)
+        YY[1::2] = np.fromstring(Y2, dtype=np.uint8)
+
+        UV = UV.reshape((image_height, image_width))
+        YY = YY.reshape((image_height, image_width))
+
+        combined_yuv_image = cv2.merge([UV, YY])
+        # YUY2 isn't the right format, but it works. Nao camera sends UYVY format (YUV422).
+        bgr_img = cv2.cvtColor(combined_yuv_image, cv2.COLOR_YUV2BGR_YUY2)
+        print("Image decode and conversion time: ", time.time() - t0)
+        return bgr_img
 
 
 if __name__ == '__main__':
-    nao = Nao(ip_address="192.168.55.145")
+    nao = Nao(ip_address="192.168.55.149")
+    model = tfapi.Model()
     try:
+        nao.connect()
+        model.load_frozen_model()
+        model.load_label_map()
+        nao.change_camera_parameters(vision_definitions.kQVGA, 9, 1)
+        nao.start_video()
+        i = 0
+        while True:
+            image = nao.get_frame()
+            with open("./test/raw_frame'{}'.dump".format(i), 'wb') as f:
+                pickle.dump(image, f)
+            image = Utility.nao_YUV2BGR(image)
+            if image is not None:
+                pass
+                image = model.predict(image_np=image)
+                t0 = time.time()
+                Utility.display_image_cv2(image)
+                i += 1
+                print "Image retrieve time 3: ", time.time() - t0
+            else:
+                nao.say("Piip poop")
+    except:
+        print ("No connection to Nao, running image data from ./test/ folder.")
         model = tfapi.Model()
         model.load_frozen_model()
         model.load_label_map()
-        nao.change_camera_parameters(vision_definitions.kQVGA, 11, 1)
-        #nao.say("I'm taking an image.")
-        nao.start_video()
-        while True:
-            image = nao.get_frame()
-            image = nao.convert_image_np(image)
+        i = 0
+        dir_len = len(os.listdir("./test/"))
+        model.start_session()
+        while i < dir_len:
+            with open("./test/raw_frame'{}'.dump".format(i), 'rb') as f:
+                image = pickle.load(f)
+            image = Utility.nao_YUV2BGR(image)
             if image is not None:
-                pass
-                #image = model.predict(image_np=image)
-                nao.display_image_cv2(image)
+                t0 = time.time()
+                image = model.predict(image_np=image)
+                print("Prediction 1 time: ", time.time() - t0)
+                Utility.display_image_cv2(image, 1)
+                i += 1
             else:
-                nao.say("Piip poop")
-                print("shit")
-    except:
+                print("Image is None, skipping...")
+    finally:
+        traceback.print_stack()
+        model.close_session()
         nao.stop_video()
-
-
 
